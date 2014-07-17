74f5c0bfe6da12df699aacdab2720902b6f1b11f
Mikhail Mutcianko <mutcianko.m@gmail.com>
2014-07-17 18:01:17 +0400

    add converter output caching
    - add two level caching: per xml conversion result and complete resulting JetFile,
      so that only modified xmls are actually converted, and if no change has been
      made to any of those, last successfully built psi file is returned
    - merge AndroidUIXmlPathProvider and AndroidUIXmlParser
    - update tests

--- compiler/cli/src/org/jetbrains/jet/cli/jvm/compiler/JetCoreEnvironment.java
+++ compiler/cli/src/org/jetbrains/jet/cli/jvm/compiler/JetCoreEnvironment.java
@@ -56,8 +56,8 @@ import org.jetbrains.jet.lang.parsing.JetParserDefinition;
 import org.jetbrains.jet.lang.parsing.JetScriptDefinitionProvider;
 import org.jetbrains.jet.lang.psi.JetFile;
 import org.jetbrains.jet.lang.resolve.CodeAnalyzerInitializer;
-import org.jetbrains.jet.lang.resolve.android.AndroidUIXmlPathProvider;
-import org.jetbrains.jet.lang.resolve.android.CliAndroidUIXmlPathProvider;
+import org.jetbrains.jet.lang.resolve.android.AndroidUIXmlParser;
+import org.jetbrains.jet.lang.resolve.android.CliAndroidUIXmlParser;
 import org.jetbrains.jet.lang.resolve.kotlin.KotlinBinaryClassCache;
 import org.jetbrains.jet.lang.resolve.kotlin.VirtualFileFinderFactory;
 import org.jetbrains.jet.lang.resolve.lazy.declarations.CliDeclarationProviderFactoryService;
@@ -245,8 +245,7 @@ public class JetCoreEnvironment {
                 configuration.getList(CommonConfigurationKeys.SCRIPT_DEFINITIONS_KEY));
 
         String s = configuration.get(JVMConfigurationKeys.ANDROID_RES_PATH);
-        project.registerService(AndroidUIXmlPathProvider.class, new CliAndroidUIXmlPathProvider(s));
-        
+        project.registerService(AndroidUIXmlParser.class, new CliAndroidUIXmlParser(s));
         project.registerService(VirtualFileFinderFactory.class, new CliVirtualFileFinderFactory(classPath));
     }
 

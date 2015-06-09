f61f6066d8c8c5b33194c895d83942c0039ddcfa
Ilya Gorbunov <ilya.gorbunov@jetbrains.com>
2015-06-09 20:00:45 +0300

    ImmutableStack sample implementation: refactor, implement more methods

--- libraries/stdlib/src/kotlin/collections/immutable/ImmutableStack.kt
+++ libraries/stdlib/src/kotlin/collections/immutable/ImmutableStack.kt
@@ -8,7 +8,7 @@ import kotlin.support.AbstractIterator
  */
 
 
-public abstract class ImmutableStack<out E> private(): ImmutableCollection<E> {
+public abstract class ImmutableStack<out E> private constructor(): ImmutableCollection<E> {
 
     public object EMPTY: ImmutableStack<Nothing>()  {
         override fun size(): Int = 0
@@ -19,25 +19,45 @@ public abstract class ImmutableStack<out E> private(): ImmutableCollection<E> {
         override fun remove(element: Any?): ImmutableStack<Nothing> = this
         override fun removeAll(c: Iterable<Any?>): ImmutableStack<Nothing> = this
         override fun retainAll(c: Iterable<Any?>): ImmutableStack<Nothing> = this
+        override fun filter(predicate: (Nothing) -> Boolean): ImmutableStack<Nothing> = this
     }
 
-    private class Cons<out E>(public val element: E, public val next: ImmutableStack<E>): ImmutableStack<E>() {
-        private val size = 1 + next.size()
+    private class Cons<out E>(public val head: E, public val tail: ImmutableStack<E>): ImmutableStack<E>() {
+        private val size = 1 + tail.size()
 
         override fun size() = size
         override fun isEmpty(): Boolean = false
-        override fun contains(o: Any?): Boolean = element == o || next.contains(element)
+        override fun contains(o: Any?): Boolean = head == o || tail.contains(head)
 
         override fun remove(element: Any?): ImmutableStack<E> {
-            if (this.element == element)
-                return next
+            if (this.head == element)
+                return tail
             else {
-                val newNext = next.remove(element)
-                return if (newNext !== next) Cons(this.element, newNext) else this
+                val newNext = tail.remove(element)
+                return if (newNext !== tail) Cons(this.head, newNext) else this
+            }
+        }
+
+        override fun filter(predicate: (E) -> Boolean): ImmutableStack<E> {
+            if (!predicate(head))
+                return tail.filter(predicate)
+            else {
+                val newNext = tail.filter(predicate)
+                return if (newNext !== tail) Cons(this.head, newNext) else this
             }
         }
 
     }
+
+
+
+    suppress("TYPE_VARIANCE_CONFLICT")
+    fun push(element: E): ImmutableStack<E> = Cons(element, this)
+
+    fun pop(): Pair<E, ImmutableStack<E>> =
+        if (this is Cons) head to tail else throw NoSuchElementException()
+
+    fun peek(): E? = if (this is Cons) head else null
 
     suppress("TYPE_VARIANCE_CONFLICT")
     override fun add(element: E): ImmutableStack<E> = Cons(element, this)
@@ -46,23 +66,27 @@ public abstract class ImmutableStack<out E> private(): ImmutableCollection<E> {
     override fun addAll(elements: Iterable<E>): ImmutableStack<E> = elements.fold(this, { c, e -> c.add(e) })
 
     override fun containsAll(c: Collection<Any?>): Boolean {
-        val set = c.toMutableSet()
+        val set = c.toHashSet()
         this.forEach { set.remove(it) }
         return set.isEmpty()
     }
 
+    protected abstract fun filter(predicate: (E) -> Boolean): ImmutableStack<E>
+
     abstract override fun remove(element: Any?): ImmutableStack<E>
 
-    override fun removeAll(c: Iterable<Any?>): ImmutableStack<E> {
-        throw UnsupportedOperationException()
-    }
+    override fun removeAll(c: Iterable<Any?>): ImmutableStack<E> = c.toHashSet().let { set -> if (set.isEmpty()) this else filter { !set.contains(it) } }
 
-    override fun retainAll(c: Iterable<Any?>): ImmutableStack<E> {
-        throw UnsupportedOperationException()
-    }
+    override fun retainAll(c: Iterable<Any?>): ImmutableStack<E> = c.toHashSet().let { set -> if (set.isEmpty()) EMPTY else filter { set.contains(it) } }
 
     override fun hashCode(): Int {
-        return super.hashCode()
+        var hash = 1
+        var next = this
+        while (next is Cons) {
+            hash = hash * 31 + next.head.hashCode()
+            next = next.tail
+        }
+        return hash
     }
 
     override fun toString(): String {
@@ -75,8 +99,8 @@ public abstract class ImmutableStack<out E> private(): ImmutableCollection<E> {
             val current = next
             when (current) {
                 is Cons -> {
-                    setNext(current.element)
-                    next = current.next
+                    setNext(current.head)
+                    next = current.tail
                 }
                 is EMPTY -> done()
             }
@@ -88,38 +112,49 @@ public abstract class ImmutableStack<out E> private(): ImmutableCollection<E> {
     suppress("TYPE_VARIANCE_CONFLICT")
     override fun builder(): ImmutableCollection.Builder<E> = Builder(this)
 
-    private class Builder<E>(private var stack: ImmutableStack<E>): AbstractCollection<E>(), ImmutableCollection.Builder<E> {
-        override fun size(): Int = stack.size()
-        override fun hashCode(): Int = stack.hashCode()
+    private class Builder<E>(override var value: ImmutableStack<E>): AbstractCollection<E>(), ImmutableCollection.Builder<E>, Mutator<ImmutableStack<E>> {
+        override fun isEmpty(): Boolean = value.isEmpty()
+        override fun size(): Int = value.size()
+        override fun hashCode(): Int = value.hashCode()
         override fun add(e: E): Boolean {
-            stack = stack.add(e)
+            value = value.add(e)
             return true
         }
 
-        override fun remove(o: Any?): Boolean {
-            val newStack = stack.remove(o)
-            if (newStack !== stack) {
-                stack = newStack
-                return true
-            }
-            return false
-        }
+        override fun remove(o: Any?): Boolean = mutate { it.remove(o) }
+        override fun removeAll(c: Collection<Any?>): Boolean = mutate { it.removeAll(c) }
+        override fun retainAll(c: Collection<Any?>): Boolean = mutate { it.retainAll(c) }
+        override fun addAll(c: Collection<E>): Boolean = mutate { it.addAll(c) }
 
         override fun clear() {
-            stack = EMPTY
+            value = EMPTY
         }
 
-        override fun contains(o: Any?): Boolean = stack.contains(o)
-        override fun containsAll(c: Collection<Any?>): Boolean = stack.containsAll(c)
-        override fun equals(other: Any?): Boolean = stack.equals(other)
+        override fun contains(o: Any?): Boolean = value.contains(o)
+        override fun containsAll(c: Collection<Any?>): Boolean = value.containsAll(c)
+        override fun equals(other: Any?): Boolean = value.equals(other)
 
-        override fun iterator(): MutableIterator<E> = object: MutableIterator<E>, Iterator<E> by stack.iterator() {
+        override fun iterator(): MutableIterator<E> = object: MutableIterator<E>, Iterator<E> by value.iterator() {
             override fun remove() {
                 // TODO: ?
                 throw UnsupportedOperationException()
             }
         }
 
-        override fun build(): ImmutableCollection<E> = stack
+        override fun build(): ImmutableCollection<E> = value
     }
+}
+
+
+private interface Mutator<T> {
+    var value: T
+}
+
+private inline fun <T> Mutator<T>.mutate(operation: (T) -> T): Boolean {
+    val newValue = operation(value)
+    if (newValue !== value) {
+        value = newValue
+        return true
+    }
+    return false
 }

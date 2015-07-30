91b594a25620f7d746ea43fd0a893aa8f9a2ddfc
Pavel V. Talanov <talanov.pavel@gmail.com>
2015-07-30 18:21:44 +0300

    Refactor: Move AnalyzerUtil to "idea-analysis" module and rewrite using new api

--- idea/idea-analysis/src/org/jetbrains/kotlin/idea/util/ShortenReferences.kt
+++ idea/idea-analysis/src/org/jetbrains/kotlin/idea/util/ShortenReferences.kt
@@ -22,7 +22,7 @@ import com.intellij.psi.PsiDocumentManager
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiRecursiveElementVisitor
 import com.intellij.psi.util.PsiTreeUtil
-import org.jetbrains.kotlin.analyzer.analyzeInContext
+import org.jetbrains.kotlin.idea.analysis.analyzeInContext
 import org.jetbrains.kotlin.descriptors.ClassDescriptor
 import org.jetbrains.kotlin.descriptors.ClassifierDescriptor
 import org.jetbrains.kotlin.descriptors.DeclarationDescriptor

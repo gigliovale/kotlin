32b1a799f3fa7003d37da0a1a40d2104b70667ab
Pavel V. Talanov <talanov.pavel@gmail.com>
2015-08-05 13:38:23 +0300

    Move ResolutionFacade to ide-common module

--- idea/src/org/jetbrains/kotlin/idea/liveTemplates/macro/BaseJetVariableMacro.java
+++ idea/src/org/jetbrains/kotlin/idea/liveTemplates/macro/BaseJetVariableMacro.java
@@ -31,7 +31,7 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.jetbrains.kotlin.descriptors.DeclarationDescriptor;
 import org.jetbrains.kotlin.descriptors.VariableDescriptor;
-import org.jetbrains.kotlin.idea.caches.resolve.ResolutionFacade;
+import org.jetbrains.kotlin.idea.resolve.ResolutionFacade;
 import org.jetbrains.kotlin.idea.caches.resolve.ResolvePackage;
 import org.jetbrains.kotlin.idea.core.IterableTypesDetection;
 import org.jetbrains.kotlin.idea.core.IterableTypesDetector;

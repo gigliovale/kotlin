890ea25fd9ce812f664c6c345232bba4174d9dfe
Zalim Bashorov <Zalim.Bashorov@jetbrains.com>
2015-09-03 22:51:13 +0300

    ~ fix choosing target when outputItem.sourceFiles is empty

--- jps-plugin/src/org/jetbrains/kotlin/jps/build/KotlinBuilder.kt
+++ jps-plugin/src/org/jetbrains/kotlin/jps/build/KotlinBuilder.kt
@@ -359,23 +359,18 @@ public class KotlinBuilder : ModuleLevelBuilder(BuilderCategory.SOURCE_PROCESSOR
 
         val representativeTarget = chunk.representativeTarget()
         for (outputItem in outputItemCollector.getOutputs()) {
-            var target: ModuleBuildTarget? = null
-            val sourceFiles = outputItem.getSourceFiles()
-            val outputFile = outputItem.getOutputFile()
-
-            if (!sourceFiles.isEmpty()) {
-                target = sourceToTarget[sourceFiles.iterator().next()]
-            }
-
-            if (target == null) {
-                target = representativeTarget
-            }
+            val sourceFiles = outputItem.sourceFiles
+            val outputFile = outputItem.outputFile
+            val target =
+                    sourceFiles.firstOrNull()?.let { sourceToTarget[it] } ?:
+                    chunk.targets.filter { it.outputDir?.let { outputFile.startsWith(it) } ?: false }.singleOrNull() ?:
+                    representativeTarget
 
             if (outputFile.getName().endsWith(".class")) {
-                result.add(GeneratedJvmClass(target!!, sourceFiles, outputFile))
+                result.add(GeneratedJvmClass(target, sourceFiles, outputFile))
             }
             else {
-                result.add(GeneratedFile(target!!, sourceFiles, outputFile))
+                result.add(GeneratedFile(target, sourceFiles, outputFile))
             }
         }
         return result
@@ -467,7 +462,8 @@ public class KotlinBuilder : ModuleLevelBuilder(BuilderCategory.SOURCE_PROCESSOR
         var recompilationDecision = DO_NOTHING
         for (generatedClass in generatedClasses) {
             if (generatedClass !is GeneratedJvmClass) {
-                incrementalCaches[generatedClass.target]!!.saveFileToCache(generatedClass.sourceFiles, generatedClass.outputFile)
+//                if (generatedClass.outputFile.parent?.parent == generatedClass.target.outputDir)
+                    incrementalCaches[generatedClass.target]!!.saveFileToCache(generatedClass.sourceFiles, generatedClass.outputFile)
                 continue
             }
 

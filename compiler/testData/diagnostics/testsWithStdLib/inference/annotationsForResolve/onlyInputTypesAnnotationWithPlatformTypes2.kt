70f7a9b0d2304165ee4b7c45ee575e9fa90d6fd3
Svetlana Isakova <svetlana.isakova@jetbrains.com>
2015-10-22 19:09:21 +0300

    ++Fixed 'OnlyInputTypes' working with platform types

--- compiler/testData/diagnostics/testsWithStdLib/inference/annotationsForResolve/onlyInputTypesAnnotationWithPlatformTypes2.kt
+++
@@ -1,9 +0,0 @@
-//!DIAGNOSTICS: -UNUSED_PARAMETER
-
-@Suppress("INVISIBLE_MEMBER", "INVISIBLE_REFERENCE")
-fun <@kotlin.internal.OnlyInputTypes T> assertEquals1(t1: T, t2: T) {}
-
-fun test(src: Array<Any>) {
-    //todo
-    assertEquals1(arrayListOf(3.0), src.filterIsInstance<Double>())
-}

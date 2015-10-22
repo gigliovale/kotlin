5dde2ea2ac9f961e9550797bb20babf70e8d7c31
Svetlana Isakova <svetlana.isakova@jetbrains.com>
2015-10-22 20:52:53 +0300

    ++

--- compiler/testData/diagnostics/tests/resolve/specialConstructions/inferenceForElvis.kt
+++ compiler/testData/diagnostics/tests/resolve/specialConstructions/inferenceForElvis.kt
@@ -3,6 +3,6 @@
 
 fun <T: Any> foo(f: (T) -> Unit): T? = null // T is used only as return type
 fun test() {
-    val x = foo { it checkType { _String() }} ?: "" // foo() is inferred as foo<String>, which isn't very good
-    val y: Any = foo { it checkType { _Any() } } ?: "" // but for now it's fixed by specifying expected type
+    val x = foo { it checkType { _<String>() }} ?: "" // foo() is inferred as foo<String>, which isn't very good
+    val y: Any = foo { it checkType { _<Any>() } } ?: "" // but for now it's fixed by specifying expected type
 }

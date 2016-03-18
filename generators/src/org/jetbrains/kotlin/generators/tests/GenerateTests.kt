2c514f8466971aa5035ed5d48b24425506fe9f45
Pavel V. Talanov <talanov.pavel@gmail.com>
2016-03-18 16:41:36 +0300

    Generate tests for resolving references to kotlin binaries from java in IDE

--- generators/src/org/jetbrains/kotlin/generators/tests/GenerateTests.kt
+++ generators/src/org/jetbrains/kotlin/generators/tests/GenerateTests.kt
@@ -540,7 +540,12 @@ fun main(args: Array<String>) {
         }
 
         testClass<AbstractReferenceResolveInJavaTest>() {
-            model("resolve/referenceInJava", extension = "java")
+            model("resolve/referenceInJava/binaryAndSource", extension = "java")
+            model("resolve/referenceInJava/sourceOnly", extension = "java")
+        }
+
+        testClass<AbstractReferenceToCompiledKotlinResolveInJavaTest>() {
+            model("resolve/referenceInJava/binaryAndSource", extension = "java")
         }
 
         testClass<AbstractReferenceResolveWithLibTest>() {

fce5c3eba67be762d40365dc957aa25d883c1891
Zalim Bashorov <Zalim.Bashorov@jetbrains.com>
2016-05-08 20:58:01 +0300

    ~~~~

--- compiler/daemon/src/org/jetbrains/kotlin/daemon/CompileServiceImpl.kt
+++ compiler/daemon/src/org/jetbrains/kotlin/daemon/CompileServiceImpl.kt
@@ -796,7 +796,7 @@ class CompileServiceImpl(
             PrintStream(
                     BufferedOutputStream(RemoteOutputStreamClient(remoteOutputStream, profiler), 4096),
                     /*autoFlush =*/ false,
-                    Charsets.UTF_8.toString()
+                    Charsets.UTF_8.name()
             )
 
     private fun createCompileServices(facade: CompilerCallbackServicesFacade, eventManager: EventManager, rpcProfiler: Profiler): Services {
--- compiler/tests/org/jetbrains/kotlin/daemon/CompilerDaemonTest.kt
+++ compiler/tests/org/jetbrains/kotlin/daemon/CompilerDaemonTest.kt
@@ -137,7 +137,7 @@ class CompilerDaemonTest : KotlinIntegrationTestBase() {
             val daemonJVMOptions = configureDaemonJVMOptions("D$COMPILE_DAEMON_LOG_PATH_PROPERTY=\"${logFile.loggerCompatiblePath}\"",
                                                              // Specify default encoding explicitly to be sure that we don't get UTF-8
                                                              // from somewhere else (e.g. inherited from parent process)
-                                                             "Dfile.encoding=MacRoman",
+                                                             "Dfile.encoding=" + Charsets.US_ASCII.name(),
                                                              inheritMemoryLimits = false, inheritAdditionalProperties = false)
 
             var daemonShotDown = false

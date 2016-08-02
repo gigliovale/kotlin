ebb64a241c6466d1594aa218b941ad14ecc81066
Dmitry Petrov <Dmitry.Petrov@jetbrains.com>
2016-08-02 16:13:48 +0300

    IR: first smoke test

--- generators/src/org/jetbrains/kotlin/generators/tests/GenerateTests.kt
+++ generators/src/org/jetbrains/kotlin/generators/tests/GenerateTests.kt
@@ -120,6 +120,7 @@ import org.jetbrains.kotlin.idea.stubs.AbstractMultiFileHighlightingTest
 import org.jetbrains.kotlin.idea.stubs.AbstractResolveByStubTest
 import org.jetbrains.kotlin.idea.stubs.AbstractStubBuilderTest
 import org.jetbrains.kotlin.integration.AbstractAntTaskTest
+import org.jetbrains.kotlin.ir.AbstractIrTextTestCase
 import org.jetbrains.kotlin.j2k.AbstractJavaToKotlinConverterForWebDemoTest
 import org.jetbrains.kotlin.j2k.AbstractJavaToKotlinConverterMultiFileTest
 import org.jetbrains.kotlin.j2k.AbstractJavaToKotlinConverterSingleFileTest
@@ -236,6 +237,10 @@ fun main(args: Array<String>) {
 
         testClass<AbstractBytecodeTextTest>() {
             model("codegen/bytecodeText")
+        }
+
+        testClass<AbstractIrTextTestCase>() {
+            model("ir/irText")
         }
 
         testClass<AbstractBytecodeListingTest>() {

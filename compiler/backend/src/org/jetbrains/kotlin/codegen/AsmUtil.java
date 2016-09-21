288189d0c5c6c5f57e5e14cd4725a8aa5362733b
Michael Bogdanov <mikhael.bogdanov@jetbrains.com>
2016-09-21 12:12:18 +0300

    infrastructure

--- compiler/backend/src/org/jetbrains/kotlin/codegen/AsmUtil.java
+++ compiler/backend/src/org/jetbrains/kotlin/codegen/AsmUtil.java
@@ -524,7 +524,7 @@ public class AsmUtil {
         mv.visitInsn(L2I);
     }
 
-    static void genInvertBoolean(InstructionAdapter v) {
+    public static void genInvertBoolean(InstructionAdapter v) {
         v.iconst(1);
         v.xor(Type.INT_TYPE);
     }

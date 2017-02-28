556004805389ac45c0b752ba2145eb38c475aea5
Pavel V. Talanov <talanov.pavel@gmail.com>
2017-02-28 16:43:00 +0300

    KtLightMethod: use dummy delegate to compute parameter count and isContructor

--- compiler/light-classes/src/org/jetbrains/kotlin/asJava/elements/KtLightMethod.kt
+++ compiler/light-classes/src/org/jetbrains/kotlin/asJava/elements/KtLightMethod.kt
@@ -64,17 +64,10 @@ class KtLightMethodImpl private constructor(
 
     override fun getContainingClass(): KtLightClass = containingClass
 
-    private val paramsList: CachedValue<PsiParameterList> by lazy(LazyThreadSafetyMode.PUBLICATION) {
-        val cacheManager = CachedValuesManager.getManager(clsDelegate.project)
-        cacheManager.createCachedValue<PsiParameterList>({
-            val parameterBuilder = LightParameterListBuilder(manager, KotlinLanguage.INSTANCE, this)
-
-            for ((index, parameter) in clsDelegate.parameterList.parameters.withIndex()) {
-                parameterBuilder.addParameter(KtLightParameter(parameter, index, this))
-            }
-
-            CachedValueProvider.Result.create(parameterBuilder, PsiModificationTracker.OUT_OF_CODE_BLOCK_MODIFICATION_COUNT)
-        }, false)
+    private val paramsList: PsiParameterList by lazy(LazyThreadSafetyMode.PUBLICATION) {
+        KtLightParameterList(this, dummyDelegate?.parameterList?.parametersCount ?: clsDelegate.parameterList.parametersCount) {
+            clsDelegate.parameterList.parameters.mapIndexed { index, clsParameter -> KtLightParameter(clsParameter, index, this@KtLightMethodImpl) }
+        }
     }
 
     private val typeParamsList: CachedValue<PsiTypeParameterList> by lazy(LazyThreadSafetyMode.PUBLICATION) {
@@ -160,7 +153,7 @@ class KtLightMethodImpl private constructor(
 
     override fun getNameIdentifier() = lightIdentifier
 
-    override fun getParameterList() = paramsList.value
+    override fun getParameterList() = paramsList
 
     override fun getTypeParameterList() = typeParamsList.value
 
@@ -258,7 +251,7 @@ class KtLightMethodImpl private constructor(
 
     override fun isVarArgs() = clsDelegate.isVarArgs
 
-    override fun isConstructor() = clsDelegate.isConstructor
+    override fun isConstructor() = dummyDelegate?.isConstructor ?: clsDelegate.isConstructor
 
     override fun getHierarchicalMethodSignature() = clsDelegate.hierarchicalMethodSignature
 

556c46535a79f493893d9d9b8c4abae71dd3ece1
Simon Ogorodnik <Simon.Ogorodnik@jetbrains.com>
2017-03-02 17:48:25 +0300

    Fix Sample reference to resolve cross-module packages correctly  #KT-14710 fixed

---
+++ idea/idea-analysis/src/org/jetbrains/kotlin/idea/kdoc/IdeSampleResolutionService.kt
@@ -0,0 +1,79 @@
+/*
+ * Copyright 2010-2017 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.jetbrains.kotlin.idea.kdoc
+
+import com.intellij.openapi.progress.ProgressManager
+import com.intellij.openapi.project.Project
+import com.intellij.psi.search.GlobalSearchScope
+import org.jetbrains.kotlin.descriptors.DeclarationDescriptor
+import org.jetbrains.kotlin.idea.caches.resolve.findModuleDescriptor
+import org.jetbrains.kotlin.idea.caches.resolve.getNullableModuleInfo
+import org.jetbrains.kotlin.idea.caches.resolve.resolveToDescriptor
+import org.jetbrains.kotlin.idea.resolve.ResolutionFacade
+import org.jetbrains.kotlin.idea.stubindex.KotlinClassShortNameIndex
+import org.jetbrains.kotlin.idea.stubindex.KotlinExactPackagesIndex
+import org.jetbrains.kotlin.idea.stubindex.KotlinFunctionShortNameIndex
+import org.jetbrains.kotlin.idea.stubindex.SubpackagesIndexService
+import org.jetbrains.kotlin.name.FqName
+import org.jetbrains.kotlin.psi.KtFile
+import org.jetbrains.kotlin.resolve.BindingContext
+import org.jetbrains.kotlin.resolve.lazy.BodyResolveMode
+
+class IdeSampleResolutionService(val project: Project) : SampleResolutionService {
+
+    override fun resolveSample(context: BindingContext, fromDescriptor: DeclarationDescriptor, resolutionFacade: ResolutionFacade, qualifiedName: List<String>): Collection<DeclarationDescriptor> {
+
+        val allScope = GlobalSearchScope.projectScope(project)
+
+        val shortName = qualifiedName.lastOrNull() ?: return emptyList()
+
+        val targetFqName = FqName.fromSegments(qualifiedName)
+
+        val functions = KotlinFunctionShortNameIndex.getInstance().get(shortName, project, allScope).asSequence()
+        val classes = KotlinClassShortNameIndex.getInstance().get(shortName, project, allScope).asSequence()
+
+        val subPackageIndex = SubpackagesIndexService.getInstance(project)
+
+        fun resolveSubpackagesRecursively(fqName: FqName): Sequence<FqName> {
+            ProgressManager.checkCanceled()
+            return subPackageIndex.getSubpackages(fqName, allScope, { true }).asSequence().let { subPackages ->
+                subPackages + subPackages.flatMap(::resolveSubpackagesRecursively)
+            }
+        }
+
+        val exactPackageIndex = KotlinExactPackagesIndex.getInstance()
+
+        val exactMembersOfPackage = exactPackageIndex.get(targetFqName.asString(), project, allScope)
+
+        val subPackages = resolveSubpackagesRecursively(targetFqName)
+
+        val packageIndexContents = exactMembersOfPackage.asSequence() +
+                                   subPackages.flatMap { exactPackageIndex.get(it.asString(), project, allScope).asSequence() }
+
+        val packageDescriptors = packageIndexContents.map(KtFile::getNullableModuleInfo)
+                .distinct()
+                .filterNotNull()
+                .map { moduleInfo -> resolutionFacade.findModuleDescriptor(moduleInfo)?.getPackage(targetFqName) }
+                .filterNotNull()
+
+        val descriptors = (functions + classes)
+                .filter { it.fqName == targetFqName }
+                .map { it.resolveToDescriptor(BodyResolveMode.PARTIAL) } // TODO Filter out not visible due dependencies config descriptors
+
+        return (descriptors + packageDescriptors).toList()
+    }
+}

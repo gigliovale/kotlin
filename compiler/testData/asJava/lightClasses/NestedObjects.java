e25f4f013e85d112e0cb3e0fdb8fcf46068405d8
Pavel V. Talanov <talanov.pavel@gmail.com>
2017-03-03 16:27:35 +0300

    Light class codegen: all objects are considered static
    Simplify code handling access flag computation
    Fix a problem where kotlin nested object wasn't producing a nested light class

---
+++ compiler/testData/asJava/lightClasses/NestedObjects.java
@@ -0,0 +1,47 @@
+public final class A {
+    public A() { /* compiled code */ }
+
+    public static final class B {
+        public B() { /* compiled code */ }
+
+        public static final class I {
+            public static final A.B.I INSTANCE;
+
+            private I() { /* compiled code */ }
+        }
+
+        public static final class II {
+            public static final A.B.II INSTANCE;
+
+            private II() { /* compiled code */ }
+        }
+    }
+
+    public static final class C {
+        public static final A.C INSTANCE;
+
+        private C() { /* compiled code */ }
+
+        public static final class D {
+            public static final A.C.D INSTANCE;
+
+            private D() { /* compiled code */ }
+
+            public static final class G {
+                public static final A.C.D.G INSTANCE;
+
+                private G() { /* compiled code */ }
+            }
+        }
+    }
+
+    public final class D {
+        public D() { /* compiled code */ }
+
+        public static final class O {
+            public static final A.D.O INSTANCE;
+
+            private O() { /* compiled code */ }
+        }
+    }
+}
---
+++ compiler/testData/asJava/lightClasses/NestedObjects.kt
@@ -0,0 +1,19 @@
+// A
+
+class A {
+    class B {
+        object I
+
+        object II
+    }
+
+    object C {
+        object D {
+            object G
+        }
+    }
+
+    inner class D {
+        object O
+    }
+}

7a292471ba4709b6370405965ff9032e6d3b04f7
Nikolay Krasko <nikolay.krasko@jetbrains.com>
2017-04-06 13:56:21 +0300

    -- fix test data

--- compiler/testData/diagnostics/tests/imports/SyntaxError.kt
+++ compiler/testData/diagnostics/tests/imports/SyntaxError.kt
@@ -24,17 +24,17 @@ class D {
 // FILE:c.kt
 import<!SYNTAX!><!> ;
 import <!SYNTAX!>.<!>
-import a.<!SYNTAX!><!> ;
+import <!PACKAGE_CANNOT_BE_IMPORTED!>a<!>.<!SYNTAX!><!> ;
 import <!SYNTAX!>.a.<!> ;
 import <!SYNTAX!>.a.b<!> ;
 import <!SYNTAX!>.a.B<!> ;
 import a.B.<!SYNTAX!><!> ;
 import a.B.C.<!SYNTAX!><!> ;
-import a.B.<!UNRESOLVED_REFERENCE!>foo<!>.<!SYNTAX!><!> ;
-import a.B.<!UNRESOLVED_REFERENCE!>bar<!>.<!SYNTAX!><!> ;
-import a.b.<!SYNTAX!><!> ;
-import a.b.c.<!SYNTAX!><!> ;
-import a.<!SYNTAX!>%<!>.b.c.<!SYNTAX!><!> ;
+import a.B.<!CANNOT_BE_IMPORTED!>foo<!>.<!SYNTAX!><!> ;
+import a.B.<!CANNOT_BE_IMPORTED!>bar<!>.<!SYNTAX!><!> ;
+import a.<!PACKAGE_CANNOT_BE_IMPORTED!>b<!>.<!SYNTAX!><!> ;
+import a.b.<!PACKAGE_CANNOT_BE_IMPORTED!>c<!>.<!SYNTAX!><!> ;
+import a.<!SYNTAX!>%<!>.b.<!PACKAGE_CANNOT_BE_IMPORTED!>c<!>.<!SYNTAX!><!> ;
 import a.b.c.D.<!SYNTAX!><!> ;
 import a.b.c.D.E.<!SYNTAX!><!> ;
 

f1314b5bf0d394845afdc9109e20aa06314a7b64
baratynskiy <baratynskiy@gmail.com>
2017-04-10 12:25:52 +0300

    Auto-generated CompileKotlinAgainstJava test group

--- generators/src/org/jetbrains/kotlin/generators/tests/GenerateTests.kt
+++ generators/src/org/jetbrains/kotlin/generators/tests/GenerateTests.kt
@@ -331,6 +331,10 @@ fun main(args: Array<String>) {
             model("compileJavaAgainstKotlin")
         }
 
+        testClass<AbstractCompileKotlinAgainstJavaTest> {
+            model("compileKotlinAgainstJava")
+        }
+
         testClass<AbstractCompileKotlinAgainstKotlinTest> {
             model("compileKotlinAgainstKotlin")
         }

96afabb13a8def8adf08c1e0ceed20b1bc85384f
Pavel V. Talanov <talanov.pavel@gmail.com>
2017-04-14 21:52:59 +0300

    Codegen: fix NPE in NO_ANNOTATION_VISITOR
    EA-100245 fixed

--- compiler/testData/asJava/lightClasses/InheritingInterfaceDefaultImpls.kt
+++ compiler/testData/asJava/lightClasses/InheritingInterfaceDefaultImpls.kt
@@ -1,6 +1,11 @@
 // p.Inheritor
 package p
 
+// annotatation added to test regression
+annotation class Anno(vararg val s: String)
+
+annotation class Bueno(anno: Anno)
+
 class Inheritor: I, I2 {
 
     fun f() {
@@ -16,9 +21,11 @@ interface I : I1 {
 }
 
 interface I1 {
+    @Bueno(Anno("G"))
     fun foo() = "foo"
 }
 
 interface I2 {
+    @Anno("S")
     fun bar() = "bar"
 }

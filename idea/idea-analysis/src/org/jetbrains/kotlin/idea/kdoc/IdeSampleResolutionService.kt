c349c2f0567ef7b0eae38e90aa19ca558e8e0ce6
Simon Ogorodnik <Simon.Ogorodnik@jetbrains.com>
2017-04-17 21:17:24 +0300

    Fix regression of sample resolution in library sources
    Use hack to answer package questions
     #KT-17448 fixed

--- idea/idea-analysis/src/org/jetbrains/kotlin/idea/kdoc/IdeSampleResolutionService.kt
+++ idea/idea-analysis/src/org/jetbrains/kotlin/idea/kdoc/IdeSampleResolutionService.kt
@@ -18,16 +18,24 @@ package org.jetbrains.kotlin.idea.kdoc
 
 import com.intellij.openapi.project.Project
 import com.intellij.psi.search.GlobalSearchScope
-import org.jetbrains.kotlin.descriptors.DeclarationDescriptor
-import org.jetbrains.kotlin.idea.caches.resolve.collectAllModuleInfosFromIdeaModel
-import org.jetbrains.kotlin.idea.caches.resolve.findModuleDescriptor
+import org.jetbrains.kotlin.descriptors.*
+import org.jetbrains.kotlin.descriptors.annotations.Annotations
 import org.jetbrains.kotlin.idea.caches.resolve.resolveToDescriptor
+import org.jetbrains.kotlin.idea.caches.resolve.resolveToDescriptorIfAny
 import org.jetbrains.kotlin.idea.resolve.ResolutionFacade
 import org.jetbrains.kotlin.idea.stubindex.KotlinClassShortNameIndex
 import org.jetbrains.kotlin.idea.stubindex.KotlinFunctionShortNameIndex
+import org.jetbrains.kotlin.idea.stubindex.PackageIndexUtil
+import org.jetbrains.kotlin.incremental.components.LookupLocation
 import org.jetbrains.kotlin.name.FqName
+import org.jetbrains.kotlin.name.Name
+import org.jetbrains.kotlin.name.isChildOf
+import org.jetbrains.kotlin.name.isOneSegmentFQN
 import org.jetbrains.kotlin.resolve.BindingContext
 import org.jetbrains.kotlin.resolve.lazy.BodyResolveMode
+import org.jetbrains.kotlin.resolve.scopes.DescriptorKindFilter
+import org.jetbrains.kotlin.resolve.scopes.MemberScope
+import org.jetbrains.kotlin.utils.Printer
 
 class IdeSampleResolutionService(val project: Project) : SampleResolutionService {
 
@@ -46,15 +54,96 @@ class IdeSampleResolutionService(val project: Project) : SampleResolutionService
                 .filter { it.fqName == targetFqName }
                 .map { it.resolveToDescriptor(BodyResolveMode.PARTIAL) } // TODO Filter out not visible due dependencies config descriptors
                 .toList()
-        if(descriptors.isNotEmpty())
+        if (descriptors.isNotEmpty())
             return descriptors
 
-        val packageDescriptors = collectAllModuleInfosFromIdeaModel(project)
-                .asSequence()
-                .map { resolutionFacade.findModuleDescriptor(it)?.getPackage(targetFqName) }
-                .filterNotNull()
-                .filterNot { it.isEmpty() }
-
-        return packageDescriptors.toList()
+        if (!targetFqName.isRoot && PackageIndexUtil.packageExists(targetFqName, GlobalSearchScope.allScope(project), project))
+            return listOf(GlobalSyntheticPackageViewDescriptor(targetFqName, project))
+        return emptyList()
     }
+}
+
+
+private class GlobalSyntheticPackageViewDescriptor(override val fqName: FqName, private val project: Project) : PackageViewDescriptor {
+    override fun getContainingDeclaration(): PackageViewDescriptor? =
+            if (fqName.isOneSegmentFQN()) null else GlobalSyntheticPackageViewDescriptor(fqName.parent(), project)
+
+
+    private val allScope = GlobalSearchScope.allScope(project)
+
+    override val memberScope: MemberScope = object : MemberScope {
+
+        override fun getContributedVariables(name: Name, location: LookupLocation): Collection<PropertyDescriptor> {
+            throw UnsupportedOperationException("Synthetic PVD for KDoc link resolution")
+        }
+
+        override fun getContributedFunctions(name: Name, location: LookupLocation): Collection<SimpleFunctionDescriptor> {
+            throw UnsupportedOperationException("Synthetic PVD for KDoc link resolution")
+        }
+
+        override fun getFunctionNames(): Set<Name> {
+            throw UnsupportedOperationException("Synthetic PVD for KDoc link resolution")
+        }
+
+        override fun getVariableNames(): Set<Name> {
+            throw UnsupportedOperationException("Synthetic PVD for KDoc link resolution")
+        }
+
+        override fun getContributedClassifier(name: Name, location: LookupLocation): ClassifierDescriptor? {
+            throw UnsupportedOperationException("Synthetic PVD for KDoc link resolution")
+        }
+
+        override fun printScopeStructure(p: Printer) {
+            p.printIndent()
+            p.print("GlobalSyntheticPackageViewDescriptorMemberScope (INDEX)")
+        }
+
+
+        fun getClassesByNameFilter(nameFilter: (Name) -> Boolean) = KotlinClassShortNameIndex.getInstance()
+                .getAllKeys(project)
+                .asSequence()
+                .filter { nameFilter(Name.identifier(it)) }
+                .flatMap { KotlinClassShortNameIndex.getInstance()[it, project, allScope].asSequence() }
+                .filter { it.fqName?.isChildOf(fqName) == true }
+                .map { it.resolveToDescriptorIfAny() }
+
+        fun getFunctionsByNameFilter(nameFilter: (Name) -> Boolean) = KotlinFunctionShortNameIndex.getInstance()
+                .getAllKeys(project)
+                .asSequence()
+                .filter { nameFilter(Name.identifier(it)) }
+                .flatMap { KotlinFunctionShortNameIndex.getInstance()[it, project, allScope].asSequence() }
+                .filter { it.fqName?.isChildOf(fqName) == true }
+                .map { it.resolveToDescriptorIfAny() }
+
+        fun getSubpackages(nameFilter: (Name) -> Boolean) =
+                PackageIndexUtil.getSubPackageFqNames(fqName, allScope, project, nameFilter).map {
+                    GlobalSyntheticPackageViewDescriptor(it, project)
+                }
+
+        override fun getContributedDescriptors(kindFilter: DescriptorKindFilter, nameFilter: (Name) -> Boolean): Collection<DeclarationDescriptor> {
+            return (getClassesByNameFilter(nameFilter)
+                    + getFunctionsByNameFilter(nameFilter)
+                    + getSubpackages(nameFilter)).filterIsInstance<DeclarationDescriptor>().toList()
+        }
+
+    }
+    override val module: ModuleDescriptor
+        get() = throw UnsupportedOperationException("Synthetic PVD for KDoc link resolution")
+    override val fragments: List<PackageFragmentDescriptor>
+        get() = throw UnsupportedOperationException("Synthetic PVD for KDoc link resolution")
+
+    override fun getOriginal() = this
+
+    private val name = fqName.shortName()
+    override fun getName(): Name = name
+
+    override fun <R : Any?, D : Any?> accept(visitor: DeclarationDescriptorVisitor<R, D>?, data: D): R {
+        throw UnsupportedOperationException("Synthetic PVD for KDoc link resolution")
+    }
+
+    override fun acceptVoid(visitor: DeclarationDescriptorVisitor<Void, Void>?) {
+        throw UnsupportedOperationException("Synthetic PVD for KDoc link resolution")
+    }
+
+    override val annotations = Annotations.EMPTY
 }

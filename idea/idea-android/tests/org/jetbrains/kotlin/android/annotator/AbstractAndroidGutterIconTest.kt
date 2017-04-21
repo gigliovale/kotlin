727de65eea5e79e5e0b136cfcf121737d92eb9be
Vyacheslav Gerasimov <vyacheslav.gerasimov@jetbrains.com>
2017-04-21 15:42:15 +0300

    Add gutter icons for Android resource references

---
+++ idea/idea-android/tests/org/jetbrains/kotlin/android/annotator/AbstractAndroidGutterIconTest.kt
@@ -0,0 +1,89 @@
+/*
+ * Copyright 2010-2017 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.jetbrains.kotlin.android.annotator
+
+import com.android.SdkConstants
+import com.intellij.openapi.util.io.FileUtil
+import com.intellij.util.PathUtil
+import com.intellij.util.ui.ColorIcon
+import junit.framework.TestCase
+import org.jetbrains.kotlin.android.KotlinAndroidTestCase
+import org.jetbrains.kotlin.idea.test.ConfigLibraryUtil
+import org.jetbrains.kotlin.test.InTextDirectivesUtils
+import org.jetbrains.kotlin.test.KotlinTestUtils
+import java.awt.Color
+import java.io.File
+import javax.swing.ImageIcon
+
+
+abstract class AbstractAndroidGutterIconTest : KotlinAndroidTestCase() {
+
+    fun doTest(path: String) {
+        val testFile = File(path)
+        val testFileText = FileUtil.loadFile(testFile)
+        val withRuntime = InTextDirectivesUtils.isDirectiveDefined(testFileText, "// WITH_RUNTIME")
+
+        val drawable = InTextDirectivesUtils.isDirectiveDefined(testFileText, "// DRAWABLE")
+        val color = InTextDirectivesUtils.findListWithPrefixes(testFileText, "// COLOR: ").takeIf { it.isNotEmpty() }?.let {
+            val components = it.map { it.toInt(16) }
+            Color(components[0], components[1], components[2])
+        }
+
+        try {
+            if (withRuntime) {
+                ConfigLibraryUtil.configureKotlinRuntime(myFixture.module)
+            }
+
+            val customManifestPath = "${testFile.path}.AndroidManifest.xml"
+            if (FileUtil.exists("$testDataPath/$customManifestPath")) {
+                deleteManifest()
+                myFixture.copyFileToProject(customManifestPath, SdkConstants.FN_ANDROID_MANIFEST_XML)
+            }
+
+            myFixture.copyDirectoryToProject(testFile.parent + "/res", "res")
+            myFixture.copyFileToProject(testFile.parent + "/R.java", "gen/${COM_MYAPP_PACKAGE_PATH}R.java")
+
+            val sourceFile = myFixture.copyFileToProject(path, "src/${PathUtil.getFileName(path)}")
+            myFixture.configureFromExistingVirtualFile(sourceFile)
+
+            val gutter = myFixture.findAllGutters().find {
+                when {
+                    drawable -> it.icon is ImageIcon
+                    color != null -> (it.icon as? ColorIcon)?.iconColor == color
+                    else -> error("COLOR or DRAWABLE must be defined!")
+                }
+            }
+
+            TestCase.assertNotNull(gutter)
+        }
+        finally {
+            if (withRuntime) {
+                ConfigLibraryUtil.unConfigureKotlinRuntime(myFixture.module)
+            }
+        }
+    }
+
+    override fun createManifest() {
+        myFixture.copyFileToProject("idea/testData/android/AndroidManifest.xml", SdkConstants.FN_ANDROID_MANIFEST_XML)
+    }
+
+    override fun getTestDataPath() = KotlinTestUtils.getHomeDirectory()
+
+    companion object {
+        private val COM_MYAPP_PACKAGE_PATH: String = "com/myapp/"
+    }
+}

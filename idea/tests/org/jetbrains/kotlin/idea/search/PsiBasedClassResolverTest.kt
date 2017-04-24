8c955c1589fd1757bf52e865397a3c8a2fe18695
Pavel V. Talanov <talanov.pavel@gmail.com>
2017-04-24 17:30:35 +0300

    PsiBasedClassResolver: use ImpreciseResolveResult instead of Boolean?

--- idea/tests/org/jetbrains/kotlin/idea/search/PsiBasedClassResolverTest.kt
+++ idea/tests/org/jetbrains/kotlin/idea/search/PsiBasedClassResolverTest.kt
@@ -108,6 +108,9 @@ class PsiBasedClassResolverTest : KotlinLightCodeInsightFixtureTestCaseBase() {
                                                                           fileText) as KtFile
         val index = fileText.indexOf(marker)
         val ref = file.findElementAt(index + marker.indexOf("Test"))!!.getParentOfType<KtSimpleNameExpression>(false)!!
-        return resolver.canBeTargetReference(ref)
+        // TODO: rewrite tests
+        val canBeTargetReference = resolver.canBeTargetReference(ref)
+        canBeTargetReference.ifSure { return it }
+        return null
     }
 }
